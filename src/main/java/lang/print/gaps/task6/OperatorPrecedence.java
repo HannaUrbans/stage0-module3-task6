6726fa64bd2467f77cac74ce00b4c45a07b64296
Hanna_Urban <huan92@yandex.ru>
2024-01-15 21:34:52 +0300

    everything except varsix=5 is corrected

--- src/main/java/lang/print/gaps/task6/OperatorPrecedence.java
+++ src/main/java/lang/print/gaps/task6/OperatorPrecedence.java
@@ -16,3 +16,5 @@ public class OperatorPrecedence {
         System.out.println(varSix);
     }
 }
+// System.out.println(firstBool && (secondBool || thirdBool) && (fourthBool || (varSix = 5) < 0));
+//        System.out.println(varSix);
